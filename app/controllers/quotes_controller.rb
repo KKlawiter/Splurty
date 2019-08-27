67d5ca48253725cd230159513dd206b11f610652
Katrina Klawiter <katrinaklawiter@gmail.com>
2019-08-27 08:13:55 -0600

    Initial commit

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,5 +1,5 @@
-class QuotesController 
-	def index
-  	@quote = Quote.order("RANDOM()").first
+class QuotesController < ApplicationController
+ 	def index
+  		@quote = Quote.order("RANDOM()").first
 	end
 end
